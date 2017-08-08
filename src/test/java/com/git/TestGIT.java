e157648de7d07fb11d5539262e5fe92b43976438
msrao <subbaraoms@gmail.com>
2017-08-08 22:40:15 +0530

    Commit1

---
+++ src/test/java/com/git/TestGIT.java
@@ -0,0 +1,20 @@
+package com.git;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestGIT {
+	
+	@Test
+	public void Login() {
+		WebDriver driver;
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://google.co.in");
+		String title = driver.getTitle();
+		System.out.println("Title is : "+title);
+
+		driver.quit();
+	}
+
+}
